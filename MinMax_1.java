9c4abd77fa00af819ab17f669507c1f3634af5e3
CesarTongoliki <49816985+CesarTongoliki@users.noreply.github.com>
2019-11-25 02:50:15 +0100

    Add files via upload

--- MinMax_1.java
+++ MinMax_1.java
@@ -14,118 +14,112 @@ public class MinMax_1
   implements Jugador, IAuto
 {
   private String nom;
-  private int my_color;
   
   public MinMax_1()
   {
-    nom = "TuPutaMadre";
+    nom = "Danésar";
   }
   
   public int moviment(Tauler t, int color)
   {
-    my_color = color;  
-    int col = (int)(t.getMida() * Math.random());
-    while (!t.movpossible(col)) {
-      col = (int)(t.getMida() * Math.random());
-    }
-    heuristica(t, color);
-    return col;                 
-  }
-  
-  public Pair <Integer,float> MinMax(Tauler t, int depth, boolean minmax){
-    LinkedList <Integer> valid_locations = get_valid_locations(t);
-    float valor = heuristica(t,my_color);
-    boolean is_terminal = valor == Float.POSITIVE_INFINITY || valor == Float.NEGATIVE_INFINITY || !t.espotmoure();
-    if (depth == 0 || is_terminal) {
-        int posible = t.espotmoure() ? 1 : 0;
-        return new Pair<>(null,valor * posible);
-    }                
-    if (minmax){
-        valor = Float.NEGATIVE_INFINITY;
-        int column = valid_locations.get((int)(valid_locations.size() * Math.random())); //valor del 0 al 7
-        for (int i = 0; i < valid_locations.size();++i){
-           int fila = get_next_fila_open(t,i);
-           Tauler taux = copy(t);
-           taux.afegeix(i, 2); // EL 2 ÉS LA FITXA DE LA IA
-           float nouValor = MinMax(taux,depth-1,false).getRight();
-           if (nouValor > valor){
-               valor = nouValor;
-               column = i;
-           }
-        }
-        return new Pair<>(column, valor);
-    } else {
-        valor = Float.POSITIVE_INFINITY;
-        int column = valid_locations.get((int)(valid_locations.size() * Math.random())); //valor del 0 al 7
-        for (int i = 0; i < valid_locations.size();++i){
-           int fila = get_next_fila_open(t,i);
-           Tauler taux = copy(t);
-           taux.afegeix(i, 1); // EL 2 ÉS LA FITXA DE LA IA
-           float nouValor = MinMax(taux,depth-1,true).getRight();
-           if (nouValor < valor){
-               valor = nouValor;
-               column = i;
-           }
-        }
-        return new Pair<>(column, valor);
-    }
-  }
-  
-  public Tauler copy(Tauler t){
-      Tauler copia = new Tauler(8);
-      int color_actual;
-      for (int i = 0; i < t.getMida();++i){
-          for (int j = 0; j < t.getMida(); ++j){
-              color_actual = t.getColor(i, j);
-              copia.afegeix(j, color_actual);
+    int col = t.getMida()/2;
+    float alpha = Float.NEGATIVE_INFINITY;
+    for(int i = 0; i < t.getMida() && t.espotmoure(); i++){
+        Tauler aux = new Tauler(t);
+          if(aux.movpossible(i)){
+              aux.afegeix(i, 1);
+              if(aux.solucio(i, 1))return i;
+              else{
+                  float nvalor = Min(aux,1,alpha,Float.POSITIVE_INFINITY,7);
+                  if(nvalor > alpha)col = i;
+                  alpha = Math.max(alpha,nvalor);
+              }
           }
-      }
-      return copia;
+    }
+    
+    int i = 0;
+    while (!t.movpossible(col) && i < t.getMida()){
+        col = i;
+        i++;
+    }
+    return col;
   }
   
-  public Integer get_next_fila_open(Tauler t, int col){
-      boolean trobat = false;
-      int i = 0;
-      while (i<t.getMida()-1 && !trobat){ //duda. porque es hasta 6 y no hasta 7?
-          if (t.getColor(i,col)==0){
-              trobat = true;
-          }
-          ++i;
+  public float Max(Tauler t, int color, float alpha, float beta, int depth){
+      if(!t.espotmoure() || depth == 0){
+          return basicHeuristica(t);
       }
-      return i;
-  }
-  
-  public LinkedList<Integer> get_valid_locations(Tauler t){
-      LinkedList<Integer> valid_loc = new LinkedList<Integer>();
       for(int i = 0; i < t.getMida(); i++){
-          if(t.movpossible(i)){
-              //t.afegeix(i, my_color);
-              valid_loc.add(i);
+          Tauler aux = new Tauler(t);
+          if(aux.movpossible(i)){
+              aux.afegeix(i, 1);
+              if(aux.solucio(i, 1))return Float.POSITIVE_INFINITY;
+              else{
+                  float nvalor = Min(aux,1,alpha,beta,depth-1);
+                  alpha = Math.max(alpha,nvalor);
+                  if(beta <= alpha)return beta;
+              }
           }
       }
-      return valid_loc;
+      return alpha;
   }
-
+  
+  public float Min(Tauler t, int color, float alpha, float beta, int depth){
+      if(!t.espotmoure() || depth == 0){
+          return heuristica(t, 1);
+      }
+      for(int i = 0; i < t.getMida(); i++){
+          Tauler aux = new Tauler(t);
+          if(aux.movpossible(i)){
+              aux.afegeix(i, -1);
+              if(aux.solucio(i, -1))return Float.NEGATIVE_INFINITY;
+              else{
+                  float nvalor = Max(aux,-1,alpha,beta,depth-1);
+                  beta = Math.min(beta, nvalor);
+                  if(beta <= alpha)return alpha;  
+              }          
+          }
+      }
+      return beta;
+  }
+  
+  public float basicHeuristica(Tauler t){
+      if(t.espotmoure() == false)return Float.NEGATIVE_INFINITY;
+      else{
+          for(int i = 0; i < t.getMida(); i++){
+              if(t.solucio(i, 1)){
+                  System.out.println("Solucio+");
+                  return Float.POSITIVE_INFINITY;
+              }
+              else if(t.solucio(i, -1)){
+                  System.out.println("Solucio-");
+                  return Float.NEGATIVE_INFINITY;
+              }
+          }
+          return (float) 0;
+      }
+  }
+  
   
   public float heuristica(Tauler t, int color){
       float max = 0;
       for(int j = 0; j < t.getMida(); j++){
-          for(int i = t.getMida()-1; i > -1 && t.getColor(t.getMida()-1,j) == 0; i--){
+          for(int i = t.getMida()-1; i > -1 && t.movpossible(i); i--){
                   if(t.getColor(i,j) == 0){
                       if(i>0)if(t.getColor(i-1,j) != 0){
-                          max+=vertical(t, j, i-1, color);
+                          max+=2*vertical(t, j, i-1, color);
                       }
                       if(j>0)if(t.getColor(i, j-1) != 0){
-                          max+=horizontalI(t,j-1,i,color);
+                          max+=2*horizontalI(t,j-1,i,color);
                       }
                       if(j<t.getMida()-1)if(t.getColor(i, j+1) != 0){
-                          max+=horizontalD(t,j+1,i,color);
+                          max+=2*horizontalD(t,j+1,i,color);
                       }
                       if(j<t.getMida()-1 && i>0)if(t.getColor(i-1,j+1) != 0){
                           max+=diagonalDchInf(t, j+1, i-1, color);
                       }
                       if(j<t.getMida()-1 && i<t.getMida()-1)if(t.getColor(i+1,j+1) != 0){
-                          max+=diagonalDchSup(t, j+1, i+1, color);
+                          max+=diagonalDchSup(t, j+1, i+1, color);                        
                       }
                       if(j>0 && i>0)if(t.getColor(i-1,j-1) != 0){
                           max+=diagonalEsqInf(t, j-1, i-1, color);
@@ -144,14 +138,13 @@ public class MinMax_1
       int color = t.getColor(alt,col);
       boolean trobat = false;
       for(int i = alt; i >= 0 && !trobat; i--){
-          if(color != t.getColor(i,col))trobat = true; //#################################################### color -> mi_color
+          if(color != t.getColor(i,col))trobat = true;
           else ++valor;
       }
       
-      valor = valor //No fichas mismo color * espacioVacio(t, col, alt+1) //cantidad de espacios vacios a partir d esa posicion; // dudaaaaaaaaaa??
-      if(valor >= 3)valor = Float.POSITIVE_INFINITY; // si la distancia a una ficha de las mias es mayor k 3 valor es inf
-      
-      if(color != mi_color && valor != 0)return -valor;
+      if(valor >= 3)valor = Float.POSITIVE_INFINITY; 
+      else Math.pow(4,valor);
+      if(color == -1 && valor != 0)return -valor;
       else return valor;
   }
   
@@ -165,9 +158,10 @@ public class MinMax_1
       }
   
       valor = valor * espacioVacio(t, col+1, alt);
-      if(valor >= 3)valor = Float.POSITIVE_INFINITY;
+      if(valor > 2)valor = Float.POSITIVE_INFINITY;
+      else Math.pow(4,valor);
       
-      if(color != mi_color && valor != 0)return -valor;
+      if(color == -1 && valor != 0)return -valor;
       else return valor;
   }
   
@@ -181,9 +175,10 @@ public class MinMax_1
       }
       
       valor = valor * espacioVacio(t, col-1, alt);
-      if(valor >= 3)valor = Float.POSITIVE_INFINITY;
+      if(valor > 2)valor = Float.POSITIVE_INFINITY;
+      else Math.pow(4,valor);
       
-      if(color != mi_color && valor != 0)return -valor;
+      if(color == -1 && valor != 0)return -valor;
       else return valor;
   }
   
@@ -199,9 +194,10 @@ public class MinMax_1
       }
       
       valor = valor * espacioVacio(t, col-1, alt+1);
-      if(valor >= 3)valor = Float.POSITIVE_INFINITY;
+      if(valor >= 4)valor = Float.POSITIVE_INFINITY;
+      else Math.pow(4,valor);
       
-      if(color != mi_color && valor != 0)return -valor;
+      if(color == -1 && valor != 0)return -valor;
       else return valor;
   }
   
@@ -217,9 +213,10 @@ public class MinMax_1
       }
       
       valor = valor * espacioVacio(t, col-1, alt-1);
-      if(valor >= 3)valor = Float.POSITIVE_INFINITY;
+      if(valor >= 4)valor = Float.POSITIVE_INFINITY;
+      else Math.pow(4,valor);
       
-      if(color != mi_color && valor != 0)return -valor;
+      if(color == -1 && valor != 0)return -valor;
       else return valor;
   }
   
@@ -235,9 +232,10 @@ public class MinMax_1
       }
       
       valor = valor * espacioVacio(t, col+1, alt+1);
-      if(valor >= 3)valor = Float.POSITIVE_INFINITY;
+      if(valor >= 4)valor = Float.POSITIVE_INFINITY;
+      else Math.pow(4,valor);
       
-      if(color != mi_color && valor != 0)return -valor;
+      if(color == -1 && valor != 0)return -valor;
       else return valor;
   }
   
@@ -253,9 +251,10 @@ public class MinMax_1
       }
       
       valor = valor * espacioVacio(t, col+1, alt-1);
-      if(valor >= 3)valor = Float.POSITIVE_INFINITY;
+      if(valor >= 4)valor = Float.POSITIVE_INFINITY;
+      else Math.pow(4,valor);
       
-      if(color != mi_color && valor != 0)return -valor;
+      if(color == -1 && valor != 0)return -valor;
       else return valor;
   }
   
@@ -269,7 +268,8 @@ public class MinMax_1
           if(t.getColor(col,i) == 0)++vuit;
           else trobat = true;
       }
-      return 1/vuit;
+      if(vuit == 0)return 1;
+      else return 1/vuit;
   }
     
   public String nom()
